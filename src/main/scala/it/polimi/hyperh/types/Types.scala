834fab1852c2ec299b7672596d558b303008bfa1
Camilo Martinez <marscamand@gmail.com>
2015-12-20 18:26:10 +0100

    Clear all previous files to have a clean jekyll root

--- src/main/scala/it/polimi/hyperh/types/Types.scala
+++
@@ -1,8 +0,0 @@
-package it.polimi.hyperh.types
-
-object Types {
-   type Permutation = Array[Int] 
-   type Constraint = (Int => Boolean)
-   type Constraints = Array[Constraint] 
-   type Value = Int
-}
