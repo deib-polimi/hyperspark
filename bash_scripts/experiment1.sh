78d01ce3f0fd6161779c2950e357378678a99d75
nemtajo <nemtajo@yahoo.com>
2016-01-21 12:29:30 +0100

    experiment1 script modified.

--- bash_scripts/experiment1.sh
+++ bash_scripts/experiment1.sh
@@ -4,17 +4,21 @@
 #clear the terminal
 clear
 declare -a instances=('1' '31' '61' '91');			#inst_ta001: 20 jobs,inst_ta031: 50 jobs,inst_ta061: 100 jobs,inst_ta091: 200 jobs
-declare -a cores=('1' '8' '16' '24' '32' '40' '48');
+declare -a cores=('1' '8' '16' '24' '32' '40');
 for i in "${instances[@]}"; do							#for each instance
 	for c in "${cores[@]}"; do							#for each number of cores to use
-		for run in $(seq 1 10); do						#run 10 times
+		for run in $(seq 1 5); do						#run 10 times
 			#execute the command
-			echo "submitting a jar... parsing application state to get application id..."
-			app_id=`grep 'tracking' <<< "$(exec spark-submit --conf spark.root.logger=INFO,console --num-executors $c --class it.polimi.hyperh.experiments.Experiment1 hyperh-0.0.1-SNAPSHOT.jar $i $c   2>&1)" | head -n1 | cut -c57-86`
-			echo $app_id
-			echo "collecting yarn logs...parsing yarn logs to get application's results..."
-			log_contents=$(grep 'CustomLogger' <<< "$(exec yarn logs -applicationId $app_id)")
-			echo "$log_contents" | sudo tee -a experiment1.txt
+			echo "submitting a jar..."
+			log_contents="`/opt/spark/bin/spark-submit --conf spark.root.logger=INFO,console --master spark://master:7077 --conf spark.cores.max=$c  --conf spark.executor.cores=1 --conf spark.executor.memory=2G --class it.polimi.hyperh.experiments.Experiment1  hyperh-0.0.1-SNAPSHOT.jar $i $c   2>&1`"
+			echo "parsing application state to get application id..."
+			app_id=`echo "$log_contents" | grep "Connected to Spark cluster with app ID" | head -n1 | cut -c100-122`
+			echo "$app_id"
+			echo "saving log to a file..."
+			filename="${app_id}.txt"
+			echo "$filename"
+			echo "$log_contents" > "$filename"
+			echo "done"
 		done
 	done
 done
---
+++ bash_scripts/runJarOnce.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+i=1
+c=24
+echo "submitting a jar..."
+log_contents="`/opt/spark/bin/spark-submit --conf spark.root.logger=INFO,console --master spark://master:7077 --conf spark.cores.max=$c --conf spark.executor.cores=1 --conf spark.executor.memory=2G --class it.polimi.hyperh.experiments.Experiment1  hyperh-0.0.1-SNAPSHOT.jar $i $c   2>&1`"
+echo "parsing application state to get application id..."
+app_id=`echo "$log_contents" | grep "Connected to Spark cluster with app ID" | head -n1 | cut -c100-122`
+echo "$app_id"
+echo "saving log to a file..."
+filename="${app_id}.txt"
+echo "$filename"
+echo "$log_contents" > "$filename"
