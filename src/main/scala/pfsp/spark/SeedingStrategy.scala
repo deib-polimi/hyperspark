fe6bf7756e0301ca85aea12ce36357dca3314ee6
nemtajo <nemtajo@yahoo.com>
2016-01-22 13:31:14 +0100

    New seeding strategy for pfsp added - FixedWindow.

--- src/main/scala/pfsp/spark/SeedingStrategy.scala
+++ src/main/scala/pfsp/spark/SeedingStrategy.scala
@@ -33,3 +33,28 @@ class SeedPlusSlidingWindow(windowSize: Int) extends SeedingStrategy {
   }
   override def usesTheSeed(): Boolean = true
 }
+class FixedWindow(windowSize: Int) extends SeedingStrategy {
+  override def divide(seedOption: Option[Solution], N: Int): Array[Option[Solution]] = {
+    val seed = seedOption.getOrElse(throw new RuntimeException("SeedingStrategySlidingWindow: None value for Option[Solution]"))
+    val perm = seed.asInstanceOf[PfsSolution].permutation
+    var array: Array[Option[Solution]] = Array()
+    for (i <- 0 until N) {
+      val windowIndex = Random.nextInt(perm.length-windowSize) //[0:perm.length-1-windowSize]
+      val window=perm.drop(windowIndex).take(windowSize)//elements until windIndex
+      val allowed=perm.filterNot(window.toSet)
+      val arrayTake = Random.shuffle(allowed.toList).toArray
+      val leftPart = arrayTake.take(windowIndex)
+      val rightPart = arrayTake.drop(windowIndex)
+      val newSol = leftPart ++ window ++ rightPart
+      array :+= Some(PfsSolution(newSol))
+    }
+    array
+  }
+  override def usesTheSeed(): Boolean = true
+}
+class SeedPlusFixedWindow(windowSize: Int) extends SeedingStrategy {
+  override def divide(seed: Option[Solution], N: Int): Array[Option[Solution]] = {
+    seed +: new FixedWindow(windowSize).divide(seed, N-1)
+  }
+  override def usesTheSeed(): Boolean = true
+}
